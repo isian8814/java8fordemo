e06f8f2be7029c67a0b8cef0cf31e4bc8141ea87
changlong min <changlong.min@neustar.biz>
2016-12-18 00:09:27 -0500

    remove build folder

--- build/resources/main/scripts/readurl.js
+++
@@ -1,6 +0,0 @@
-var feed = 'http://services.explorecalifornia.org/rss/tours.php';
-var url = new java.net.URL(feed);
-input = new java.util.Scanner(url.openStream());
-input.useDelimiter('$')
-var contents = input.next()
-contents
